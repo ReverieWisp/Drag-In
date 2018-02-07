603e6a0ebe2757c3cf0bc90a291fe5cdc7b79bf3
Reverie Wisp <36029079+ReverieWisp@users.noreply.github.com>
2018-02-06 23:32:17 -0800

    Updated

--- Source/main.cpp
+++ Source/main.cpp
@@ -9,9 +9,14 @@ Allows for dragging things into a key-reading console.
 
 @copyright See LICENSE.md
 *****************************************************************************/
-#include <iostream>                // std::cout
-#include <RUtils/RTimekeeper.hpp>  // Rutils::RException
-#include <ConsoleInput/console-input.h>
+#include <iostream>                     // std::cout
+#include <string>                       // std::string
+#include <ConsoleInput/console-input.h> // KeyHit, GetChar
+
+// Defines
+#define NO_INPUT 0
+
+
 
 // Some function to process key presses.
 void ProcessKey(char c)
@@ -19,20 +24,70 @@ void ProcessKey(char c)
   std::cout << "A key was pressed! It was: " << c << '\n';
 }
 
+// Some function to process path drag-ins.
+void ProcessFilepath(std::string path)
+{
+  std::cout << "Woah, a path! It was: " << path << '\n';
+}
 
 // Application entry point
 int main(int argc, char** argv)
 {
-
+  int last = NO_INPUT;
+  std::string buffer = "";
 
   while (1)
   {
-    while (int val = KeyHit())
+    int hit = KeyHit();
+    if (hit)
+      do
+      {
+        last = GetChar();
+        buffer += last;
+      } while (hit = KeyHit());
+    else
+      if (last != NO_INPUT)
+      {
+        if (buffer.size() > 1)
+          ProcessFilepath(buffer);
+        else
+          ProcessKey(last);
+
+        last = NO_INPUT;
+        buffer.clear();
+      }
+  }
+}
+      /*
+    if (val == NO_INPUT)
     {
-      ProcessKey(GetChar());
+      if (buffer.size() > 0)
+      {
+        ProcessFilepath(buffer);
+        buffer.clear();
+      }
+
+      last = NO_INPUT;
     }
+    else
+      while (val)
+      {
+        if (last != NO_INPUT)
+        {
+          last = GetChar();
+          buffer += last;
+        }
+        else
+        {
+          last = GetChar();
+          ProcessKey(last);
+        }
+
+        val = KeyHit();
+      }
   }
 
   // Normal termination
   return 0;
 }
+*/
